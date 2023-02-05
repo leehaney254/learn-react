fa47887a11a6506731a76bc4529a140bdc8af715
Leehaney gee <georgeleehaney20@gmail.com>
2023-02-05 21:41:00 +0300

    Styles: Styling react in css

---
+++ css_styling/src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
